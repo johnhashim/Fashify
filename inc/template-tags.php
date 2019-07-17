7d002dde0406f551b1abfd99b29f0ff29eb2f0a2
john hashim <johnnyhashim@gmail.com>
2019-07-16 22:33:39 -0400

    initail commit

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -236,7 +236,7 @@ if ( ! function_exists( 'fashify_footer_site_info' ) ) {
     {
         ?>
 		<div class="theme-info-text">
-        	<?php printf( esc_html__( 'also member of %1$s', 'fashify' ), '<a href="https://dimeforum.com" rel="nofollow">dimeforum</a>' ); ?>
+        	<?php printf( esc_html__( 'Made with  %1$s', 'fashify' ), '<a href="https://blaqdu.com" rel="nofollow">Love</a>' ); ?>
 		</div>
 		<?php
     }
