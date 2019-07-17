a57256373638861ccf4fc5a5f02a1dc2d5b6b4ef
john hashim <johnnyhashim@gmail.com>
2019-07-16 22:05:32 -0400

    initail commit

--- sidebar.php
+++ sidebar.php
@@ -14,4 +14,42 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <aside id="secondary" class="sidebar widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<style>
+.ad{
+	height: auto;
+    padding: 9px 5px;
+    margin: auto;
+    box-shadow: 0px 0px 5px #777;
+    background: #fff;
+    overflow: hidden;
+	border: 1px solid #eee;
+	border-radius: 4px;
+	max-width: 300px;	
+	text-align: center;
+}
+.advert{
+	padding:50px;
+}
+
+@media only screen and (max-width: 768px) {
+  /* For mobile phones: */
+  .advertize{
+	display:none;
+}
+}
+
+</style>
+<div class="advert">
+<div class="ad">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 970by250 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-7616051391644176"
+     data-ad-slot="7650038623"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+</div>
 </aside><!-- #secondary -->
